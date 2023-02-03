89b65ddaef9e0e0575fc49d94ed9c2160a78c753
Jonny Burger <jonathanburger11@gmail.com>
2023-02-03 18:24:52 +0100

    reducer state

--- hooks/useLambda.ts
+++ hooks/useLambda.ts
@@ -26,8 +26,6 @@ export const useLambda = (
     renderId: "",
   });
 
-  const [status, setStatus] = useState<"rendering" | "done" | "error">();
-
   const init = (type: "still" | "media") => {
     setState({
       price: undefined,
@@ -44,7 +42,7 @@ export const useLambda = (
     if (status === "rendering") return alert("Already rendering");
     init("still");
     const result = await postStill(id, inputProps);
-    if (!result) return setStatus("error");
+    if (!result) return setState((s) => ({ ...s, status: "error" }));
     setState({
       progress: 1,
       status: "done",
@@ -53,13 +51,13 @@ export const useLambda = (
       renderId: result.renderId,
       url: getUrl(result.renderId),
     });
-  }, [id, inputProps, status]);
+  }, [id, inputProps]);
 
   const renderMedia = useCallback(async () => {
     if (status === "rendering") return alert("Already rendering");
     init("media");
     const result = await postMedia(id, inputProps);
-    if (!result) return setStatus("error");
+    if (!result) return setState((s) => ({ ...s, status: "error" }));
     setState({
       price: null,
       progress: 0,
@@ -68,27 +66,28 @@ export const useLambda = (
       renderId: result.renderId,
       type: "media",
     });
-  }, [id, inputProps, status]);
+  }, [id, inputProps]);
 
   useEffect(() => {
     if (status === "rendering" && state.renderId && state.type === "media") {
       const interval = setInterval(async () => {
         try {
           const result = await getProgress(state.renderId);
-          if (!result) return setStatus("error");
-          if (result.fatalErrorEncountered) setStatus("error");
+          if (!result) return setState((s) => ({ ...s, status: "error" }));
+          if (result.fatalErrorEncountered)
+            setState((s) => ({ ...s, status: "error" }));
           setState((s) => ({
             ...s,
             price: result.costs.accruedSoFar,
           }));
-          if (result.done) setStatus("done");
+          if (result.done) setState((s) => ({ ...s, status: "done" }));
           setState((s) => ({
             ...s,
             progress: s.status === "rendering" ? result.overallProgress : 1,
           }));
         } catch (e) {
           console.error(e);
-          setStatus("error");
+          setState((s) => ({ ...s, status: "error" }));
         }
       }, refreshInterval);
       return () => {
