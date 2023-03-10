9282c93f7c51ada31a42e18a94680fa09a14eee3
Jonny Burger <jonathanburger11@gmail.com>
2023-03-10 14:38:51 +0100

    nice spinner

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Spacing } from "../Spacing";
+import { Spinner } from "../Spinner/Spinner";
 import styles from "./styles.module.css";
 
 export const Button: React.FC<{
   onClick: () => void;
   disabled?: boolean;
   children: React.ReactNode;
-}> = ({ onClick, disabled, children }) => {
+  loading?: boolean;
+}> = ({ onClick, disabled, children, loading }) => {
   return (
     <button className={styles.button} onClick={onClick} disabled={disabled}>
+      {loading && (
+        <>
+          <Spinner size={20}></Spinner>
+          <Spacing></Spacing>
+        </>
+      )}
       {children}
     </button>
   );
