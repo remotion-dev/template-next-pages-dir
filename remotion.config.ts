ea690a9f7e56e6d63aa0a937cdaf201921459e51
Jonny Burger <jonathanburger11@gmail.com>
2023-03-10 11:01:53 +0100

    add tailwind back for now

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,7 +1,10 @@
-import {Config} from 'remotion';
-import {webpackOverride} from './webpack-override';
+// See all configuration options: https://remotion.dev/docs/config
+// Each option also is available as a CLI flag: https://remotion.dev/docs/cli
 
-Config.Rendering.setImageFormat('jpeg');
-Config.Output.setOverwriteOutput(true);
+// Note: The configuration file does only apply if you render via the CLI !
 
-Config.Bundling.overrideWebpackConfig(webpackOverride);
+import { Config } from "remotion";
+import { webpackOverride } from "./webpack-override";
+
+Config.setImageFormat("jpeg");
+Config.overrideWebpackConfig(webpackOverride);
