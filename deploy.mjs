ba6e5ca78e255cdf41e9920e90628d6afc2e64b7
Jonny Burger <jonathanburger11@gmail.com>
2023-03-10 17:36:12 +0100

    deploy script

--- deploy.mjs
+++ deploy.mjs
@@ -1,15 +1,32 @@
-import { deployFunction } from "@remotion/lambda";
+import {
+  deployFunction,
+  deploySite,
+  getOrCreateBucket,
+} from "@remotion/lambda";
 import dotenv from "dotenv";
-import { RAM, REGION, TIMEOUT } from "./config.mjs";
+import path from "path";
+import { RAM, REGION, SITE_NAME, TIMEOUT } from "./config.mjs";
 
 dotenv.config();
 
 await deployFunction({
-  architecture: "arm64",
   createCloudWatchLogGroup: true,
   memorySizeInMb: RAM,
   region: REGION,
   timeoutInSeconds: TIMEOUT,
+  architecture: "arm64",
+});
+console.log("Function ensured");
+
+const { bucketName } = await getOrCreateBucket({
+  region: REGION,
+});
+console.log("Bucket ensured");
+await deploySite({
+  bucketName,
+  entryPoint: path.join(process.cwd(), "remotion", "index.ts"),
+  siteName: SITE_NAME,
+  region: REGION,
 });
 
-console.log("Function deployed");
+console.log("Site ensured");
