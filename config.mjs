f277defa042213a072a0e0f66c032dc2736e0bc5
Jonny Burger <jonathanburger11@gmail.com>
2023-03-15 14:16:50 +0100

    explain when to redeploy

--- config.mjs
+++ config.mjs
@@ -1,7 +1,7 @@
 import { VERSION } from "remotion/version";
 
 export const SITE_NAME = "my-next-app";
-export const REGION = "eu-west-2";
+export const REGION = "us-east-1";
 export const RAM = 2048;
 export const DISK = 2048;
 export const TIMEOUT = 240;
--- deploy.mjs
+++ deploy.mjs
@@ -7,26 +7,38 @@ import dotenv from "dotenv";
 import path from "path";
 import { RAM, REGION, SITE_NAME, TIMEOUT } from "./config.mjs";
 
+process.stdout.write("Deploying Lambda function... ");
+
 dotenv.config();
 
-await deployFunction({
+const { alreadyExisted, functionName } = await deployFunction({
   createCloudWatchLogGroup: true,
   memorySizeInMb: RAM,
   region: REGION,
   timeoutInSeconds: TIMEOUT,
   architecture: "arm64",
 });
-console.log("Function ensured");
+console.log(functionName, alreadyExisted ? "(already existed)" : "(created)");
 
+// TODO: Report if already existed: https://github.com/remotion-dev/remotion/pull/1936
+process.stdout.write("Ensuring bucket... ");
 const { bucketName } = await getOrCreateBucket({
   region: REGION,
 });
-console.log("Bucket ensured");
-await deploySite({
+console.log(bucketName);
+
+process.stdout.write("Deploying site... ");
+const { siteName } = await deploySite({
   bucketName,
   entryPoint: path.join(process.cwd(), "remotion", "index.ts"),
   siteName: SITE_NAME,
   region: REGION,
 });
 
-console.log("Site ensured");
+console.log(siteName);
+
+console.log();
+console.log("You now have everything you need to render videos!");
+console.log("Re-run this command when:");
+console.log("   1) you changed the video template");
+console.log("   2) you upgraded Remotion");
