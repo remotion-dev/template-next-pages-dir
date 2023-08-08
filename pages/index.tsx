54fac08ad436d719915b71a93d33beb85dd00777
Jonny Burger <jonathanburger11@gmail.com>
2023-08-08 15:54:39 +0200

    don't zoom in on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,6 +46,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Remotion and Next.js</title>
         <meta name="description" content="Remotion and Next.js" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={container}>
