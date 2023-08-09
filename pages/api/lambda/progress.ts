491b2482c018b5187dfa70155fac8a3ba3e61e02
Jonny Burger <jonathanburger11@gmail.com>
2023-08-09 11:20:59 +0200

    disable edge again, due to crypto

--- pages/api/lambda/progress.ts
+++ pages/api/lambda/progress.ts
@@ -7,10 +7,6 @@ import { DISK, RAM, REGION, TIMEOUT } from "../../../config.mjs";
 import { executeApi } from "../../../helpers/api-response";
 import { ProgressRequest, ProgressResponse } from "../../../types/schema";
 
-export const config = {
-  runtime: "edge",
-};
-
 const progress = executeApi<ProgressResponse, typeof ProgressRequest>(
   ProgressRequest,
   async (req, body) => {
